96660e57f10a98cf365e2bc00c43e42da3a4edce
jorunska <jorunskalnes@icloud.com>
2024-01-20 18:47:13 +0100

    updates to text and logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col sm p-32 bg-gradient-to-r from-gray-50 to-purple-50">
+    <main className="flex min-h-screen flex-col sm p-32 bg-gradient-to-r from-gray-50 to-purple-50 mx-auto">
       <div className="lg:flex">
        
         <div className="">
@@ -14,7 +14,7 @@ export default function Home() {
           >
             {' '}
             <Image
-              src="/public/signature_2.svg"
+              src="/signature_2.png"
               alt="Joruns Logo"
               width={100}
               height={24}
@@ -24,8 +24,8 @@ export default function Home() {
         </div>
       </div>
 
-      <div className='w-4/5'>
-        <h1>Engajsert interaksjonsdesigner</h1>
+      <div className="w-4/5">
+        <h1>Engasjert interaksjonsdesigner</h1>
         <p>Jeg brenner for å designe løsninger som gjør hverdagen enklere for alle!</p>
       </div>
 
